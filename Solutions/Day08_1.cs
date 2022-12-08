7f73f60eb9850c15e8552dc4ebd0e401fcf6f746
Benjamin E. Iserman <winggar1228@gmail.com>
2022-12-07 23:53:33 -0600

    day 8

--- Solutions/Day08_1.cs
+++ Solutions/Day08_1.cs
@@ -12,7 +12,56 @@ internal static class Day08_1
 			.Select(s => s)
 			.ToList();
 
+		var trees = new Dictionary<(int, int), (bool vis, int t)>();
+
+		for (int i = 0; i < input.Count; i++)
+		{
+			for (int j = 0; j < input[0].Length; j++)
+			{
+				trees[(i, j)] = (i == 0 || i == input.Count - 1 || j == 0 || j == input[0].Length - 1, input[i][j] - '0');
+			}
+		}
+
 		var result = 0;
+		
+		for (int i = 1; i < input.Count - 1; i++)
+		{
+			for (int j = 1; j < input[0].Length - 1; j++)
+			{
+				bool upFlag = false, rightFlag = false, leftFlag = false , downFlag = false;
+
+				for (int k = 1; k < Math.Max(input.Count, input[0].Length); k++)
+				{
+					if (i - k >= 0 && trees[(i - k, j)].t >= trees[(i, j)].t)
+					{
+						leftFlag = true;
+					}
+
+					if (i + k < input.Count && trees[(i + k, j)].t >= trees[(i, j)].t)
+					{
+						rightFlag = true;
+					}
+
+					if (j - k >= 0 && trees[(i, j - k)].t >= trees[(i, j)].t)
+					{
+						downFlag = true;
+					}
+
+					if (j + k < input[0].Length && trees[(i, j + k)].t >= trees[(i, j)].t)
+					{
+						upFlag = true;
+					}
+				}
+
+				if (!leftFlag || !rightFlag || !upFlag || !downFlag)
+				{
+					trees[(i, j)] = (true, trees[(i, j)].t);
+				}
+			}
+		}
+
+		result = trees.Count(x => x.Value.vis);
+		
 
 		return result.ToString();
 	}
--- Solutions/Day08_2.cs
+++ Solutions/Day08_2.cs
@@ -12,7 +12,77 @@ internal static class Day08_2
 			.Select(s => s)
 			.ToList();
 
+		var trees = new Dictionary<(int, int), (bool vis, int t)>();
+
+		for (int i = 0; i < input.Count; i++)
+		{
+			for (int j = 0; j < input[0].Length; j++)
+			{
+				trees[(i, j)] = (i == 0 || i == input.Count - 1 || j == 0 || j == input[0].Length - 1, input[i][j] - '0');
+			}
+		}
+
 		var result = 0;
+
+		List<int> scores = new();
+
+		for (int i = 1; i < input.Count - 1; i++)
+		{
+			for (int j = 1; j < input[0].Length - 1; j++)
+			{
+				bool upFlag = false, rightFlag = false, leftFlag = false, downFlag = false;
+				int a = 0, b = 0, c = 0, d = 0;
+
+				for (int k = 1; k < Math.Max(input.Count, input[0].Length); k++)
+				{
+					if (j - k < 0) leftFlag = true;
+					if (j + k >= input[0].Length) rightFlag = true;
+					if (i - k < 0) downFlag = true;
+					if (i + k >= input.Count) upFlag = true;
+
+					if (!leftFlag)
+					{
+						a++;
+						if (trees[(i, j - k)].t >= trees[(i, j)].t)
+						{
+							leftFlag = true;
+						}
+					}
+
+					if (!rightFlag)
+					{
+						b++;
+						if (trees[(i, j + k)].t >= trees[(i, j)].t)
+						{
+							rightFlag = true;
+						}
+					}
+
+					if (!downFlag)
+					{
+						c++;
+						if (trees[(i - k, j)].t >= trees[(i, j)].t)
+						{
+							downFlag = true;
+						}
+					}
+
+					if (!upFlag)
+					{
+						d++;
+						if (trees[(i + k, j)].t >= trees[(i, j)].t)
+						{
+							upFlag = true;
+						}
+					}
+				}
+
+				scores.Add(a * b * c * d);
+			}
+		}
+
+		//result = trees.Count(x => x.Value.vis);
+		result = scores.Max();
 
 		return result.ToString();
 	}
