7d9d914299f1459bb112e147c530a86f6c37454f
Benjamin E. Iserman <winggar1228@gmail.com>
2022-12-10 06:31:51 -0600

    day 10

--- Solutions/Day10_1.cs
+++ Solutions/Day10_1.cs
@@ -13,6 +13,38 @@ internal static class Day10_1
 			.ToList();
 		dynamic result = 0;
 
+		var strengths = new Dictionary<int, int>();
+		void AddStrength(int i, int x)
+		{
+			if (i % 20 == 0)
+			{
+				//Console.WriteLine($"{i}: {i * x}");
+				strengths.Add(i, i * x);
+			}
+		}
+
+		int cycle = 0;
+		int x = 1;
+		foreach (var line in input)
+		{
+			var split = line.Split();
+			if (split[0] == "noop")
+			{
+				cycle++;
+				AddStrength(cycle, x);
+			}
+			else if (split[0] == "addx")
+			{
+				cycle++;
+				AddStrength(cycle, x);
+				cycle++;
+				AddStrength(cycle, x);
+				x += int.Parse(split[1]);
+			}
+		}
+
+		result = strengths[20] + strengths[60] + strengths[100] + strengths[140] + strengths[180] + strengths[220];
+
 		return result.ToString();
 	}
 }
--- Solutions/Day10_2.cs
+++ Solutions/Day10_2.cs
@@ -5,13 +5,51 @@ internal static class Day10_2
 {
 	public static string Execute()
 	{
-		//var input = Input.ListUntilWhiteSpace(s => int.Parse(s), Program.GetLineOfInput);
 		var input = Program.GetAllInput()
 			.Replace("\r", string.Empty)
 			.Split("\n")
 			.Select(s => s)
 			.ToList();
 		dynamic result = 0;
+
+		var strengths = new Dictionary<int, int>();
+		void AddStrength(int i, int x)
+		{
+			if (Math.Abs(x - ((i - 1) % 40)) <= 1)
+			{
+				Console.Write("#");
+			}
+			else
+			{
+				Console.Write(" ");
+			}
+
+			if (i % 40 == 0)
+			{
+				Console.WriteLine();
+				strengths.Add(i, i * x);
+			}
+		}
+
+		int cycle = 0;
+		int x = 1;
+		foreach (var line in input)
+		{
+			var split = line.Split();
+			if (split[0] == "noop")
+			{
+				cycle++;
+				AddStrength(cycle, x);
+			}
+			else if (split[0] == "addx")
+			{
+				cycle++;
+				AddStrength(cycle, x);
+				cycle++;
+				AddStrength(cycle, x);
+				x += int.Parse(split[1]);
+			}
+		}
 
 		return result.ToString();
 	}
