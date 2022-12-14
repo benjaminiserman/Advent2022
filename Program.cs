e1dcbc9720e56c8053d0e87927e01e0d4374039c
Benjamin E. Iserman <winggar1228@gmail.com>
2022-12-13 23:21:54 -0600

    day 14

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ public static class Program
 	[STAThread]
 	public static void Main()
 	{
-		var result = Day14_1.Execute();
-		//var result = Day14_2.Execute();
+		var result = Day15_1.Execute();
+		//var result = Day15_2.Execute();
 
 		Console.WriteLine(result);
 		Clipboard.SetText(result.ToString());
--- Solutions/Day14_1.cs
+++ Solutions/Day14_1.cs
@@ -11,7 +11,109 @@ internal static class Day14_1
 			.Split("\n")
 			.Select(s => s)
 			.ToList();
-		dynamic result = 0;
+
+		Dictionary<(int x, int y), char> dict = new();
+
+		static void Set<T1, T2>(Dictionary<T1, T2> dict, T1 key, T2 value)
+		{
+			if (dict.ContainsKey(key))
+			{
+				dict[key] = value;
+			}
+			else
+			{
+				dict.Add(key, value);
+			}
+		}
+
+		bool IsOccupied((int x, int y) t)
+		{
+			if (dict.ContainsKey(t))
+			{
+				return dict[t] != ' ';
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		(int x, int y) ParseTuple(string s)
+		{
+			var split = s.Split(",");
+			var x = int.Parse(split[0]);
+			var y = int.Parse(split[1]);
+			return (x, y);
+		}
+
+		foreach (var line in input)
+		{
+			var split = line.Split(" -> ");
+			var start = ParseTuple(split[0]);
+			foreach (var following in split[1..])
+			{
+				var tuple = ParseTuple(following);
+				for (var i = Math.Min(start.x, tuple.x); i <= Math.Max(start.x, tuple.x); i++)
+				{
+					for (var j = Math.Min(start.y, tuple.y); j <= Math.Max(start.y, tuple.y); j++)
+					{
+						dict.TryAdd((i, j), '#');
+					}
+				}
+
+				start = tuple;
+			}
+		}
+
+		var bottom = dict.Keys.Max(t => t.y);
+		bool cont = true;
+		int count = 0;
+		while (cont)
+		{
+			count++;
+			Set(dict, (500, 0), 'O');
+			var sandPos = (x: 500, y: 0);
+			
+			bool Try(int x, int y)
+			{
+				if (!IsOccupied((sandPos.x + x, sandPos.y + y)))
+				{
+					Set(dict, sandPos, ' ');
+					Set(dict, (sandPos.x + x, sandPos.y + y), 'O');
+					sandPos = (sandPos.x + x, sandPos.y + y);
+					return true;
+				}
+
+				return false;
+			}
+
+			while (true)
+			{
+				var test = Try(0, 1);
+				if (!test)
+				{
+					test = Try(-1, 1);
+				}
+
+				if (!test)
+				{
+					test = Try(1, 1);
+				}
+
+				if (sandPos.y >= bottom)
+				{
+					cont = false;
+					break;
+				}
+
+				if (!test)
+				{
+					break;
+				}
+			}
+		}
+
+		dynamic result = count - 1;
 
 		return result.ToString();
 	}
